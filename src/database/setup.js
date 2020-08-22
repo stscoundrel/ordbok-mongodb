91cef9732e5694233403f7b8a6dc7f348d6bc41f
stscoundrel <sampo@pixels.fi>
2020-08-22 19:26:38 +0300

    DB setup: add getClient as export

--- src/database/setup.js
+++ src/database/setup.js
@@ -4,14 +4,24 @@ const { MongoClient } = require('mongodb')
  * Setup DB & Collection.
  */
 const setupDB = async ({ url, dbName, collectionName }) => {
-  const client = await MongoClient.connect(url, { useUnifiedTopology: true })
+  const client = getClient(url)
   const collections = await client.db(dbName).listCollections().toArray()
 
   const hasCollection = collections.filter((collection) => collection.name === collectionName)
 
+  // Create collection, if not exists.
   if (hasCollection.length === 0) {
     await client.db(dbName).createCollection(collectionName)
   }
+
+  return client
+}
+
+/**
+ * Get DB client instance.
+ */
+const getClient = async (url) => {
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true })
 
   return client
 }
@@ -27,5 +37,6 @@ const populateDB = async (client, config, words) => {
 
 module.exports = {
   setupDB,
+  getClient,
   populateDB,
 }
